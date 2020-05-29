77532167bdfa9aa547a19e3b52d3f08183e7481f
Agnieszka Wojtara <aganawed@me.com>
2020-05-29 18:46:57 +0200

    js finished

--- car.js
+++ car.js
@@ -11,22 +11,22 @@ function calculateInsurance () {
 
 if (whichCountry == 'Austria') {	
 //insurance = horse_power x 100 / age   + 50 
-	var insurance = Math.round(Number((customerHorsepower * 100) / customerAge + 50));
-
+	var insurance = Math.round(Number(customerHorsepower * 100 / customerAge + 50);
+	document.getElementById('calculationResult').innerHTML = `Hi ${customerName}, your insurance for ${whichCountry} costs ${insurance} €.`
 	
 }
 else if (whichCountry == 'Greece') {
 // insurance = horse_power x 150 / (age+3)  + 50;
-	var insurance = Math.round(Number(customerHorsepower * 150) / (customerAge + 3) + 50);
-	
+	var insurance = Math.round(Number(customerHorsepower * 150 / (customerAge + 3) + 50));
+	document.getElementById('calculationResult').innerHTML = `Hi ${customerName}, your insurance for ${whichCountry} costs ${insurance} €.`
 }
 else if (whichCountry == 'Hungary') {
 // insurance = horse_power x 120 / age  + 100;
-	var insurance = Math.round(Number(customerHorsepower * 120) / customerAge + 100);
-	
+	var insurance = Math.round(Number(customerHorsepower * 120 / customerAge + 100));
+	document.getElementById('calculationResult').innerHTML = `Hi ${customerName}, your insurance for ${whichCountry} costs ${insurance} €.`
 }
 else {
-
+	document.getElementById('calculationResult').innerHTML = 'Please select a country'
 };
 }
 
