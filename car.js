96619f58c342aea2b9b29fd32f7bfabe052cbd28
Agnieszka Wojtara <aganawed@me.com>
2020-05-29 15:18:35 +0200

    js added

---
+++ car.js
@@ -0,0 +1,13 @@
+if (document.getElementById('country').value == 'Austria') {
+
+}
+else if (document.getElementById('country').value == 'Greece') {
+
+}
+else if (document.getElementById('country').value == 'Hungary') {
+
+}
+else {
+
+};
+
